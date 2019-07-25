1807be0323890f1b5e88f2346159385d740706d1
alexfernando <alex.fernando.801@gmail.com>
2019-07-25 02:21:50 +0000

    logic-change

--- src/componentes/Button.js
+++ src/componentes/Button.js
@@ -11,7 +11,7 @@ class Button extends Component {
     })
 
     render() {
-        console.log(this.isOperator)
+        
         return ( 
             <div className={`button-symbol ${
                 this.isOperator ? "color-operator" : ""
